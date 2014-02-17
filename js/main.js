e6c838bb564bb06ca94691d722d36c21247831fe
Tammy Soo <tammysoo@gmail.com>
2014-02-16 21:16:24 -0800

    added sections with scrolls and clickable pig parts

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+$('#ham').click(function () {aboutParts('#aboutHam')});
+$('#loin').click(function () {aboutParts('#aboutLoin')});
+$('#butt').click(function () {aboutParts('#aboutButt')});
+$('#jowl').click(function () {aboutParts('#aboutJowl')});
+$('#shoulder').click(function () {aboutParts('#aboutShoulder')});
+$('#belly').click(function () {aboutParts('#aboutBelly')});
+$('#ribs').click(function () {aboutParts('#aboutRibs')});
+$('.button').click(backToPig);
+
+
+function aboutParts(pigPart) {
+  console.log('aboutHam clicked: ' + pigPart);
+  event.preventDefault();
+  $(pigPart).siblings().removeClass('active').hide();
+  $(pigPart).show().addClass('active');
+  $('body').animate({scrollTop:$(pigPart).position().top}), 3000;
+}
+
+function backToPig() {
+  $('#aboutHam').hide().siblings().removeClass('active').hide();
+  $('body').animate({scrollTop:$('#pig').position().top}), 3000;
+}
